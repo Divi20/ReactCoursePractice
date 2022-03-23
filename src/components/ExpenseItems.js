1a9ad0ef1d7a2312d18be0831d846b884545e2f5
Divyangi <divyangisingh@outlook.com>
2022-03-23 12:37:34 +0530

    build custom component

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,8 @@
+//a component must resturn a jsx
+// cannot be return - error asking to return something 
+// return null will work
+function ExpenseItems() {
+    return <h2>abc</h2>  
+}
+
+export default ExpenseItems;
