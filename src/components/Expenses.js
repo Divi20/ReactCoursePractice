6a179338e493aca4e3d18f522da0a1ad7ba212bb
Divyangi <divyangisingh@outlook.com>
2022-03-24 11:52:24 +0530

    added css to expenses

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,7 +1,8 @@
 import ExpenseItems from "./ExpenseItems";
+import './Expenses.css';
 function Expenses( props) {
     const expenses = props.expenses;
-    return <div>
+    return <div className="card expenses">
     <ExpenseItems title = {expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
       <ExpenseItems title = {expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
       <ExpenseItems title = {expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
